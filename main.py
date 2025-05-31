03e7904342dde048abdba6ecc45874010967a391
AnnaPov19 <annamaria.pricop@student.upt.ro>
2025-05-31 19:24:08 +0300

    Update main.py
    using firestore instead of datastore

--- main.py
+++ main.py
@@ -3,38 +3,38 @@ from google.cloud import firestore
 import datetime
 
 app = Flask(__name__)
-client = datastore.Client(datastore="bilete")
+client = firestore.Client()
 
 @app.route('/ticket/<ticket_id>', methods=['GET'])
 def get_ticket(ticket_id):
-    key = client.key('Bilet', ticket_id)
-    entity = client.get(key)
+    doc_ref = client.collection("Bilet").document(ticket_id)
+    entity = doc_ref.get()
 
-    if not entity:
+    if not entity.exists:
         return jsonify({'message': 'Biletul nu există'}), 404
 
-    ticket_date = datetime.datetime.strptime(entity['data'], '%d.%m.%Y').date()
+    ticket_data = entity.to_dict()
+    ticket_date = datetime.datetime.strptime(ticket_data['data'], '%d.%m.%Y').date()
     current_date = datetime.date.today()
 
     if ticket_date < current_date:
         return jsonify({'valid': False, 'message': 'Bilet expirat'})
     else:
-        return jsonify({'valid': True, 'bilet': dict(entity)})
+        return jsonify({'valid': True, 'bilet': ticket_data})
 
 @app.route('/ticket', methods=['POST'])
 def create_ticket():
     data = request.get_json()
     required = ['data', 'destinație', 'nume_client', 'ora', 'plecare']
+    
     if not all(k in data for k in required):
         return jsonify({'message': 'Date incomplete'}), 400
 
     new_id = f"bilet-{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}"
-    key = client.key('Bilet', new_id)
-    entity = datastore.Entity(key=key)
-    entity.update({**data, 'id': new_id})
-    client.put(entity)
+    doc_ref = client.collection("Bilet").document(new_id)
+    doc_ref.set({**data, 'id': new_id})
 
     return jsonify({'message': 'Bilet creat', 'id': new_id}), 201
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=8080)
